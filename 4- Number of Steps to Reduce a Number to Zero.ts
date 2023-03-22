a92e006e90b391b52d87404b84d191876f502d93
nvtung194208 <tung.nv194208@sis.hust.edu.vn>
2023-03-22 19:17:27 +0700

    easy

---
+++ 4- Number of Steps to Reduce a Number to Zero.ts
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+
+function numberOfSteps(num: number): number {
+    let count = 0;
+ 
+    while(num !=0){
+        if(num%2==0){
+            num = num/2
+            count++
+        }
+        else {    
+            num = num-1;
+            count ++    
+        }
+    }
+ 
+    return count
+ 
+ };

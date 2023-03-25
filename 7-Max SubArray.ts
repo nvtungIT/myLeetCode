310eda488482cca4cb306980e9359cd9b67c7ca5
nvtungIT <tung.nv.work@gmail.com>
2023-03-26 01:55:04 +0700

    only need to return the maxSum , use 1 loop to update the sum

---
+++ 7-Max SubArray.ts
@@ -0,0 +1,13 @@
+function maxSubArray(nums: number[]): number {
+    let local = 0
+    let global = -Infinity
+  
+    for (const num of nums) {
+      local = Math.max(num, local + num)
+      if (local > global) { global = local }
+      console.log(num ,  local)
+    }
+  
+    return global
+  }
+  
